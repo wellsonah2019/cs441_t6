182c90547b7d074d4b9d4f03a4b4789500e7586e
Al <bkup.12@protonmail.com>
2022-04-08 19:21:15 +0800

    postexploit part 2

--- basic/post.py
+++ basic/post.py
@@ -1,17 +1,20 @@
 class State:
   def __init__(self) -> None:
-      self.state = "0"
+    # self.state = str([line.strip() for line in open("post.txt", 'r')][0])
+    return None
 
   def __str__(self) -> str:
-    return self.state
+    return str([line.strip() for line in open("post.txt", 'r')][0])
   
+  def resetstate(self):
+    with open("post.txt", "w") as f:
+      f.write("0")
+
   def getstate(self):
-    return self.state
+    return str([line.strip() for line in open("post.txt", 'r')][0])
 
   def changestate(self, state):
-    if isinstance(state, int):
-      self.state = state
-    else:
-      self.state = state
+    with open("post.txt", "w") as f:
+      f.write(state)
 
 post_exploit_state = State()
